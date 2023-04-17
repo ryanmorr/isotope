3b29eb446832f69ff9d305660471ecd1de0f287a
Ryan Morr <rm.morr@gmail.com>
2023-04-17 16:10:20 -0400

    Convert to use classes for performance

--- src/isotope.js
+++ src/isotope.js
@@ -1,52 +1,140 @@
-function Store(value) {
-    this._value = value;
-    this._subscribers = [];
-}
-
-Store.prototype.value = function() {
-    return this._value;
-};
-
-Store.prototype.set = function(value) {
-    const prev = this._value;
-    this._value = value;
-    this._subscribers.slice().forEach((subscriber) => subscriber(value, prev));
-    return this._value;
-};
-
-Store.prototype.update = function(callback) {
-    return this.set(callback(this._value));
-}
-
-Store.prototype.subscribe = function(callback) {
-    if (!this._subscribers.includes(callback)) {
-        this._subscribers.push(callback);
-        callback(this._value);
-        return () => {
-            const index = this._subscribers.indexOf(callback);
-            if (index !== -1) {
-                this._subscribers.splice(index, 1);
-            }
-        };
+export class Store {
+    constructor(value) {
+        this._value = value;
+        this._subscribers = [];
     }
-};
 
-Store.prototype.then = function(resolve) {
-    resolve(this._value);
-};
+    value() {
+        return this._value;
+    }
 
-Store.prototype.toString = function() {
-    return String(this._value);
-};
+    set(value) {
+        const prev = this.value();
+        this._value = value;
+        this._subscribers.slice().forEach((callback) => callback(value, prev));
+        return value;
+    }
 
-Store.prototype.valueOf = function() {
-    return this._value;
-};
+    subscribe(callback) {
+        const subscribers = this._subscribers;
+        if (!subscribers.includes(callback)) {
+            subscribers.push(callback);
+            callback(this.value());
+            return () => {
+                const index = subscribers.indexOf(callback);
+                if (index !== -1) {
+                    subscribers.splice(index, 1);
+                }
+            };
+        }
+    }
 
-Store.prototype.toJSON = function() {
-    return this._value;
-};
+    then(resolve) {
+        resolve(this.value());
+    }
+    
+    toString() {
+        return String(this.value());
+    }
+    
+    valueOf() {
+        return this.value();
+    }
+    
+    toJSON() {
+        return this.value();
+    }
+}
+
+class ValueStore extends Store {
+    update(callback) {
+        return this.set(callback(this.value()));
+    }
+}
+
+class DerivedStore extends Store {
+    constructor(deps, callback) {
+        super();
+        let initialized = false;
+        const values = [];
+        const sync = () => super.set(callback(...values));
+        deps.forEach((dep, i) => dep.subscribe((value) => {
+            values[i] = value;
+            if (initialized) {
+                sync();
+            }
+        }));
+        initialized = true;
+        sync();
+    }
+}
+
+class AsyncDerivedStore extends Store {
+    constructor(deps, callback) {
+        super();
+        let count = 0;
+        let initialized = false;
+        const values = [];
+        const sync = (setter) => callback(...values.concat([setter]));
+        deps.forEach((dep, i) => dep.subscribe((value) => {
+            values[i] = value;
+            if (initialized) {
+                count++;
+                const n = count;
+                sync((val) => {
+                    if (count === n) {
+                        super.set(val);
+                    }
+                });
+            }
+        }));
+        initialized = true;
+        sync((val) => super.set(val));
+    }
+}
+
+class ReducerStore extends Store {
+    constructor(initialState, reducer) {
+        super(initialState);
+        this._reducer = reducer;
+    }
+
+    dispatch(action) {
+        return super.set(this._reducer(this.value(), action));
+    }
+}
+
+DerivedStore.prototype.set = undefined;
+AsyncDerivedStore.prototype.set = undefined;
+ReducerStore.prototype.set = undefined;
 
 export function store(value) {
-    return new Store(value);
+    return new ValueStore(value);
 }
+
+export function derived(...deps) {
+    const callback = deps.pop();
+    if (callback.length > deps.length) {
+        return new AsyncDerivedStore(deps, callback);
+    }
+    return new DerivedStore(deps, callback);
+}
+
+export function reducer(initialState, reducer) {
+    return new ReducerStore(initialState, reducer);
+}
+
+export function effect(...deps) {
+    let initialized = false;
+    const callback = deps.pop();
+    const values = [];
+    const unsubscribers = deps.map((dep, i) => dep.subscribe((value) => {
+        values[i] = value;
+        if (initialized) {
+            callback(...values);
+        }
+    }));
+    initialized = true;
+    callback(...values);
+    return () => unsubscribers.forEach((unsubscribe) => unsubscribe());
+} 
--- test/specs/store.js
+++ test/specs/store.js
@@ -1,45 +1,47 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { store } from '../../src/isotope.js';
+import { store, Store } from '../../src/isotope.js';
 
 describe('store', () => {
-    it('should return the internal value', () => {
+    it('should encapsulate a value', () => {
         const foo = store();
         const bar = store(123);
         
+        expect(foo).to.be.an.instanceof(Store);
         expect(foo.value()).to.equal(undefined);
+        expect(bar).to.be.an.instanceof(Store);
         expect(bar.value()).to.equal(123);
     });
 
     it('should set the internal value and return the new value', () => {
-        const value = store('foo');
+        const foo = store('foo');
         
-        expect(value.value()).to.equal('foo');
+        expect(foo.value()).to.equal('foo');
 
-        expect(value.set('bar')).to.equal('bar');
-        expect(value.value()).to.equal('bar');
+        expect(foo.set('bar')).to.equal('bar');
+        expect(foo.value()).to.equal('bar');
 
-        expect(value.set('baz')).to.equal('baz');
-        expect(value.value()).to.equal('baz');
+        expect(foo.set('baz')).to.equal('baz');
+        expect(foo.value()).to.equal('baz');
     });
 
     it('should update the internal value with a callback function and return the new value', () => {
-        const value = store(1);
+        const foo = store(1);
         
-        expect(value.value()).to.equal(1);
+        expect(foo.value()).to.equal(1);
 
-        expect(value.update((val) => val + 10)).to.equal(11);
-        expect(value.value()).to.equal(11);
+        expect(foo.update((val) => val + 10)).to.equal(11);
+        expect(foo.value()).to.equal(11);
 
-        expect(value.update((val) => val + 100)).to.equal(111);
-        expect(value.value()).to.equal(111);
+        expect(foo.update((val) => val + 100)).to.equal(111);
+        expect(foo.value()).to.equal(111);
     });
 
     it('should call a subscriber immediately when added', () => {
-        const value = store();
+        const foo = store();
         
         const spy = sinon.spy();
-        value.subscribe(spy);
+        foo.subscribe(spy);
 
         expect(spy.callCount).to.equal(1);
         expect(spy.args[0][0]).to.equal(undefined);
@@ -47,23 +49,108 @@ describe('store', () => {
     });
 
     it('should call subscribers when the internal value changes', () => {
-        const value = store(10);
+        const foo = store(10);
         
         const spy = sinon.spy();
-        value.subscribe(spy);
+        foo.subscribe(spy);
 
         expect(spy.callCount).to.equal(1);
         expect(spy.args[0][0]).to.equal(10);
         expect(spy.args[0][1]).to.equal(undefined);
 
-        value.set(20);
+        foo.set(20);
         expect(spy.callCount).to.equal(2);
         expect(spy.args[1][0]).to.equal(20);
         expect(spy.args[1][1]).to.equal(10);
 
-        value.update((val) => val + 100);
+        foo.update((val) => val + 100);
         expect(spy.callCount).to.equal(3);
         expect(spy.args[2][0]).to.equal(120);
         expect(spy.args[2][1]).to.equal(20);
+    });
+
+    it('should not allow the same function to subscribe more than once', () => {
+        const foo = store('foo');
+
+        const spy = sinon.spy();
+
+        foo.subscribe(spy);
+        foo.subscribe(spy);
+        foo.subscribe(spy);
+
+        expect(spy.callCount).to.equal(1);
+
+        foo.set('baz');
+
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should remove a subscriber', () => {
+        const foo = store('foo');
+
+        const spy = sinon.spy();
+        const unsubscribe = foo.subscribe(spy);
+
+        expect(spy.callCount).to.equal(1);
+
+        foo.set('bar');
+
+        expect(spy.callCount).to.equal(2);
+        
+        unsubscribe();
+        foo.set('baz');
+
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should allow subscribers to remove themselves without disrupting others', () => {
+        const foo = store('foo');
+
+        let unsubscribe;
+        let doUnsubscribe = false;
+
+        const spy1 = sinon.spy();
+        const spy2 = sinon.spy(() => {
+            if (doUnsubscribe) {
+                unsubscribe();
+            }
+        });
+        const spy3 = sinon.spy();
+
+        foo.subscribe(spy1);
+        unsubscribe = foo.subscribe(spy2);
+        foo.subscribe(spy3);
+
+        expect(spy1.callCount).to.equal(1);
+        expect(spy2.callCount).to.equal(1);
+        expect(spy3.callCount).to.equal(1);
+
+        foo.set('bar');
+
+        expect(spy1.callCount).to.equal(2);
+        expect(spy2.callCount).to.equal(2);
+        expect(spy3.callCount).to.equal(2);
+        
+        doUnsubscribe = true;
+        foo.set('baz');
+
+        expect(spy1.callCount).to.equal(3);
+        expect(spy2.callCount).to.equal(3);
+        expect(spy3.callCount).to.equal(3);
+
+        foo.set('qux');
+
+        expect(spy1.callCount).to.equal(4);
+        expect(spy2.callCount).to.equal(3);
+        expect(spy3.callCount).to.equal(4);
+    });
+
+    it('should support implicit type conversions', async () => {
+        const foo = store(10);
+        
+        expect(foo.toString()).to.equal('10');
+        expect(foo.valueOf()).to.equal(10);
+        expect(foo.toJSON()).to.equal(10);
+        expect(await foo).to.equal(10);
     });
 });
